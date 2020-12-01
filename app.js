6600dddb1bf362972c65cee7f486dac06f990303
unknown <dashaborzova@yandex.ru>
2020-12-01 23:32:44 +0300

    finished structure

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-$(document).ready(function () {
-    $('.nav-button').click(function () {
+$(document).ready(function() {
+    $('.nav-button').click(function() {
         $('.nav-button').toggleClass('change');
-    });
 
-    $(window).scroll(function () {
-        let position = $(this).scrollTop();
-        if (position >= 200) {
-            $('.nav-menu').addClass('custom-navbar');
-        } else {
-            $('.nav-menu').removeClass('custom-navbar');
-        }
     });
 });
 
+$(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position >= 200) {
+        $('.nav-menu').addClass('custom-navbar');
+    } else {
+        $('.nav-menu').removeClass('custom-navbar');
+    }
+});
+
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true, //прокручивание элементов по кругу
+
+        nav: false, //отобразить элементы управления
+        margin: 40,
+        dots: false,
+        smartSpeed: 2000,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+});
